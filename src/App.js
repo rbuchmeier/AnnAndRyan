7b0e435f2ca42f563a338633141f8c53a4bf9490
Ryan Buchmeier <rwbleo@gmail.com>
2022-01-16 11:48:05 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import storm from './storm.jpg';
+import butte from './Butte.jpg';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <div className="App-body">
+        <img style={{width:"100%", marginTop:"20px"}} src={butte} alt="Butte" />
+      </div>
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="App-header">
+            <h1>Ann and Ryan</h1>
+            <nav style={{marginLeft:"auto"}}>
+                <ul>
+                    <li><a href="#">Wedding</a></li>
+                    <li><a href="#">Lander</a></li>
+                    <li><a href="#">Chattanooga</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
