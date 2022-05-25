99e7a7abc174317fabd66e4b5b752251adac225e
Ryan Buchmeier <rwbleo@gmail.com>
2022-05-24 22:25:51 -0600

    Adjust width

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,3 @@
-import Main from "../components/Main";
 import butte from "../Butte.jpg";
 import Section from "../components/Section";
 
@@ -8,7 +7,9 @@ function Home() {
       <img style={{width:"100%", marginTop:"20px"}} src={butte} alt="Butte" />
       <Section title="We are getting married!!">
         <h3>Welcome to our Wedding Website!</h3>
-        <p>We will be married on Memorial Day, May 30, 2022 at a small private ceremony in northern Wyoming. We wanted a small wedding, and we also wanted to celebrate with as many of our friends and family as would like to join us! So to compromise, we planned a private wedding followed by two receptions: a wedding celebration in Lander, WY on July 10, 2022, and a smaller, informal reception in the Chattanooga area, TN on October 9, 2022. The Lander one is the main event, and we added the Chattanooga one as an option to make travel easier for our eastern friends and family. RSVP to either one, or both! We can’t wait to hang out with you.</p>
+        <div className="Text-body">
+          <p>We will be married on Memorial Day, May 30, 2022 at a small private ceremony in northern Wyoming. We wanted a small wedding, and we also wanted to celebrate with as many of our friends and family as would like to join us! So to compromise, we planned a private wedding followed by two receptions: a wedding celebration in Lander, WY on July 10, 2022, and a smaller, informal reception in the Chattanooga area, TN on October 9, 2022. The Lander one is the main event, and we added the Chattanooga one as an option to make travel easier for our eastern friends and family. RSVP to either one, or both! We can’t wait to hang out with you.</p>
+        </div>
       </Section>
     </div>
   )
